bc5e0f1047263816f4864374ed74ebe17c9185b7
fastnotes-user <fastnotes@example.com>
2025-06-24 19:16:04 +0200

    fix: Remove unused mode parameter in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    optimizeDeps: {
-      include: ['tailwindcss', 'postcss']
-    }
+export default defineConfig(() => ({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['tailwindcss', 'postcss']
   }
-})
+}))
